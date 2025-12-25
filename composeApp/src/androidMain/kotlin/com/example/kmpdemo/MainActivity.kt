5d6db1c30cdc50115ba4899dbcf85b148b92f402
qiulong <qiulong@360.cn>
2025-12-25 15:57:37 +0800

    基础组件在compose中的替代物

--- composeApp/src/androidMain/kotlin/com/example/kmpdemo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/kmpdemo/MainActivity.kt
@@ -4,8 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,7 +23,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -27,8 +42,97 @@ class MainActivity : ComponentActivity() {
 //            App()
             // 1.4-组合、重组作用域和 remember()
             test_1_4()
+            // 基础组件在compose中的替代物
+            test_1_5()
         }
     }
+}
+
+@Composable
+private fun test_1_5() {
+    val nums = (1..100).toList()
+    Column(Modifier.safeContentPadding()) {
+        var name by remember { mutableStateOf("邱珑") }
+        Text(
+            text = name,
+            modifier = Modifier
+                .safeContentPadding()
+                .background(Color.Blue),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+        )
+        Image(painterResource(R.drawable.ic_launcher_background), "头像")
+        Button({
+            name = "点了"
+        }) {
+            Text(
+                text = name,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+            )
+        }
+        // LinearLayout == Column、Row
+        // FrameLayout == Box、setContent摆放规则跟Box类似
+        // RelativeLayout == Row + Column + Box
+        // RecycleView = LazyColumn、LazyRow
+        LazyColumn(
+            Modifier
+                .background(Color.Green)
+                .height(200.dp)
+        ) {
+            item {
+                Text(
+                    text = "Top",
+                )
+            }
+            items(nums.size) {
+                Text(
+                    text = "第 $it 项",
+                )
+            }
+            item {
+                Text(
+                    text = "Bottom",
+                )
+            }
+        }
+        // ScrollView == Modifier.verticalScroll
+        Column(
+            Modifier
+                .background(Color.Green)
+                .height(200.dp)
+                // 这个方法写在最后才有效
+                .verticalScroll(rememberScrollState())
+
+        ) {
+            Text(
+                text = "Top",
+            )
+            for (num in nums) {
+                Text(
+                    text = "第 $num 项",
+                )
+            }
+            Text(
+                text = "Bottom",
+            )
+        }
+        // ViewPage == HorizontalPager()
+        HorizontalPager(
+            modifier = Modifier
+                .background(Color.Red)
+                .height(50.dp), state = rememberPagerState() { 2 }) {
+            Text(
+                text = "Top",
+            )
+            Text(
+                text = "Bottom",
+            )
+        }
+    }
+
 }
 
 @Composable
@@ -66,7 +170,8 @@ private fun MainActivity.test_1_4() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+//    App()
+    test_1_5()
 }
 
 @Composable
