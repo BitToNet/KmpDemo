e4b023b0ab0607caebbb33111088f85c420339a7
qiulong <qiulong@360.cn>
2025-12-25 11:03:35 +0800

    by和remember的用法

--- composeApp/src/androidMain/kotlin/com/example/kmpdemo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/kmpdemo/MainActivity.kt
@@ -38,7 +38,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MainActivity.test_1_4() {
     Column(Modifier.safeContentPadding()) {
+        // by 方法是为了提供getValue和setvalue功能，防止每次传递mutableState，而是传递的mutableState.value
+        // var name by mutableStateOf("邱珑")
+        // var name = mutableStateOf("邱珑")
+        // by和=的区别：
+        // 1、=传递的是mutableState对象，
+        //      by传递的是mutableState.value，所以使用by生成的name的时候,直接用 Text(name)
+        //      而使用=生成的name的时候,需要加.value Text(name.value)
+        // 2.第二点不同是
+        //      =生成的name，他的监听可以传递，
+        //      by不行，比如 val name1 = name; name1改变的时候，不会引发重组
+
+        // remember 是为了在name值被修改的时候，引发页面重组，导致name的初始化重复执行
         // 包成函数类型对象，需要时再执行
+        // remember起到缓存作用，第一次调用我的时候，我会去执行，第二次调用会把缓存给你
         var name by remember { mutableStateOf("邱珑") }
 
         Button({}) {
